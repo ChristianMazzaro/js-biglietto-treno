5b187536ff7cbd326c3873621772175d5e4f6214
ChristianMazzaro <christianmazzaro2@gmail.com>
2023-06-12 17:08:48 +0200

    Esercizio Completato

--- JS/script.js
+++ JS/script.js
@@ -13,35 +13,56 @@ if (age < 18) {
     Sconto = (Prezzo * 20) / 100;
     console.log(Sconto, typeof Sconto, 'Sconto');
 
-    let Prezzo_Finale = 0;
-    Prezzo_Finale = (Prezzo - Sconto);
-    console.log(Prezzo_Finale, typeof Prezzo_Finale, 'Prezzo_Finale');
+    let Prezzo_Scontato = 0;
+    Prezzo_Scontato = (Prezzo - Sconto);
+    console.log(Prezzo_Scontato, typeof Prezzo_Scontato, 'Prezzo_Scontato');
+    
+    const Prezzo_Finale = document.getElementById("prezzofinale");
+    console.log(Prezzo_Finale, typeof Prezzo_Finale, 'Prezzo_Finale');  
 
+    Prezzo_Finale.innerHTML =  Prezzo_Finale.innerHTML + Prezzo_Scontato;
+
+    Math. floor(Prezzo_Scontato);
 }
 
 else if (age > 65) {
 
     let Sconto = 0;
     Sconto = (Prezzo * 40) / 100;
-    console.log(Sconto);
+    console.log(Sconto, typeof Sconto, 'Sconto');
 
-    let Prezzo_Finale = 0;
-    Prezzo_Finale = (Prezzo - Sconto);
-    console.log(Prezzo_Finale, typeof Prezzo_Finale, 'Prezzo_Finale');
+    let Prezzo_Scontato = 0;
+    Prezzo_Scontato = (Prezzo - Sconto);
+    console.log(Prezzo_Scontato, typeof Prezzo_Scontato, 'Prezzo_Scontato');
+    
+
+    const Prezzo_Finale = document.getElementById("prezzofinale");
+    console.log(Prezzo_Finale, typeof Prezzo_Finale, 'Prezzo_Finale');  
+
+    Prezzo_Finale.innerHTML =  Prezzo_Finale.innerHTML + Prezzo_Scontato;
+
+    Math. floor(Prezzo_Scontato);
+
 }
 
 else if (18 > age < 65) {
 
     let Sconto = 0;
-    console.log(Sconto);
+    console.log(Sconto, typeof Sconto, 'Sconto');
 
-    let Prezzo_Finale = 0;
-    Prezzo_Finale = (Prezzo - Sconto);
-    console.log(Prezzo_Finale, typeof Prezzo_Finale, 'Prezzo_Finale');
+    let Prezzo_Scontato = 0;
+    Prezzo_Scontato = (Prezzo - Sconto);
+    console.log(Prezzo_Scontato, typeof Prezzo_Scontato, 'Prezzo_Scontato');
+    
+    const Prezzo_Finale = document.getElementById("prezzofinale");
+    console.log(Prezzo_Finale, typeof Prezzo_Finale, 'Prezzo_Finale');  
+
+    Prezzo_Finale.innerHTML =  Prezzo_Finale.innerHTML + Prezzo_Scontato;
+
+    Math. floor(Prezzo_Scontato);
 }
 
 
-Math. floor(Prezzo_Finale);
 
 
 
